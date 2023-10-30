3e0870c5624bc3e44fbd80b40f1d43e1e550887b
carlos enrique <cguzmanc10@miumg.edu.gt>
2023-10-29 20:36:23 -0600

    proyecto 85%

--- pf2023/src/App.js
+++ pf2023/src/App.js
@@ -1,33 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react'; // Importa 'useState' desde React
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';  // Importa 'Routes' y 'Route'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import Inicio from './componentes/Inicio';
 import SobreMi from './componentes/SobreMi';
 import Proyectos from './componentes/Proyectos';
-import Contacto from './componentes/Contactos';  // Asegúrate de que el nombre del archivo coincida con la ubicación.
+import Contacto from './componentes/Contactos';
 
 function App() {
+  const [paginaActual, setPaginaActual] = useState('Inicio'); // Estado para rastrear la página actual
   return (
     <Router>
       <div className="App">
         <ul className="navbar">
           <li>
-            <Link to="/Inicio">Inicio</Link>  {/* Corrige las rutas aquí */}
+            <Link to="/Inicio" onClick={() => setPaginaActual('Inicio')} className={paginaActual === 'Inicio' ? 'active' : ''}>
+              Inicio
+            </Link>
           </li>
           <li>
-            <Link to="/SobreMi">SobreMi</Link>
+            <Link to="/SobreMi" onClick={() => setPaginaActual('SobreMi')} className={paginaActual === 'SobreMi' ? 'active' : ''}>
+              Sobre Mi
+            </Link>
           </li>
           <li>
-            <Link to="/Proyectos">Proyectos</Link>
+            <Link to="/Proyectos" onClick={() => setPaginaActual('Proyectos')} className={paginaActual === 'Proyectos' ? 'active' : ''}>
+              Proyectos
+            </Link>
           </li>
           <li>
-            <Link to="/Contactos">Contacto</Link>
+            <Link to="/Contactos" onClick={() => setPaginaActual('Contactos')} className={paginaActual === 'Contactos' ? 'active' : ''}>
+              Contacto
+            </Link>
           </li>
         </ul>
 
         <Routes>
-          <Route path="/Inicio" element={<Inicio />} />  {/* Corrige las rutas aquí */}
+          <Route path="/Inicio" element={<Inicio />} />
           <Route path="/SobreMi" element={<SobreMi />} />
           <Route path="/Proyectos" element={<Proyectos />} />
           <Route path="/Contactos" element={<Contacto />} />
